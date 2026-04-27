8b758376c450644a57a58836da712ccdf6d0621f
ShivamGupta15 <infosys.shivam@gmail.com>
2026-04-27 16:54:18 +0530

    added baseURl to api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  #baseURL: '/api',
+  baseURL: 'https://ramainn.onrender.com/' ,
   headers: {
     'Content-Type': 'application/json',
   },
