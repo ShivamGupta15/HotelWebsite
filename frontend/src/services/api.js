26744adcac72b92d570d1eba2c11ce7020b07c21
ShivamGupta15 <infosys.shivam@gmail.com>
2026-04-27 17:00:13 +0530

    added origin to server.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  #baseURL: '/api',
+  
   baseURL: 'https://ramainn.onrender.com/' ,
   headers: {
     'Content-Type': 'application/json',
